fff77b1461d9064bdff2d0df99cd47cfa7589fc1
Piyush289kumar <piyushraikwar289@gmail.com>
2026-05-02 22:01:59 +0530

    Implement JWT token generation and email extraction methods

--- src/main/java/com/restcrudv1/util/JwtUtil.java
+++ src/main/java/com/restcrudv1/util/JwtUtil.java
@@ -1,10 +1,34 @@
 package com.restcrudv1.util;
 
+import java.security.Key;
+import java.util.Date;
+
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 public class JwtUtil {
 	
 	private final String JWT_SECRET = "myjwtsecretkey";
 	private final Key key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes());
+	
+	public String generateToken(String email) {
+		
+		return Jwts.builder()
+				.subject(email)
+				.issuedAt(new Date())
+				.expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+				.signWith(key)
+				.compact();
+	}
+	
+	public String extractEmail(String token) {
+		
+		return Jwts.parser()
+				.verifyWith((javax.crypto.SecretKey) key)
+				.build()
+				.parseSignedClaims(token)
+				.getPayload()
+				.getSubject();
+	}
 
 }
